58b6db45a5c2705577dd5025933c9ab8ce18289a
Evan Woods <j.evan.woods@gmail.com>
2024-07-27 19:47:53 -0400

    Created First Recipe
    Created the first recipe as well as the functions to bring up the pop-up and to close the popup.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello, world')
+console.log('connected');
+
+function openRecipe (val) {
+    console.log('open recipe ran')
+    const targetDiv = document.getElementById(val);
+    targetDiv.classList.remove('hidden');
+  }
+
+  function closeRecipe (val) {
+    console.log(val);
+    const targetDiv = document.getElementById(val);
+    targetDiv.classList.add('hidden');
+  }
